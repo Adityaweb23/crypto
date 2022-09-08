de0412f321d474cbb8563a23b770beaf07762feb
Harsh jangitwar <harshjangitwar@Harshs-MacBook-Air.local>
2022-09-08 14:04:09 +0530

    improve readability

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,37 @@
-import React from 'react'
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { useGlobalContext } from "../Context";
 const Navbar = () => {
-  const {query,setQuery}=useGlobalContext();
+  const { query, setQuery } = useGlobalContext();
   return (
     <nav className="navbar navbar-expand-lg navbar-dark shadow-5-strong sticky-top">
-  <div className="container-fluid">
-    <NavLink className="navbar-brand" to='/'>CRYPTO</NavLink>
-    <form className="d-flex input-group w-auto" onSubmit={(e)=>e.preventDefault()}>
-      <input
-        
-        type="search"
-        className="form-control rounded  bg-dark text-white"
-        placeholder="Search"
-        aria-label="Search"
-        aria-describedby="search-addon"
-        value={query} 
-        onChange={e=>setQuery(e.target.value)}
-      />
-      <span className="input-group-text border-none bg-dark" id="search-addon">
-        <i className="fas fa-search text-white"></i>
-      </span>
-    </form>
-  </div>
-</nav>
-  )
-}
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          CRYPTO MATRIX
+        </NavLink>
+        <form
+          className="d-flex input-group w-auto"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            type="search"
+            className="form-control rounded  bg-dark text-white"
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="search-addon"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <span
+            className="input-group-text border-none bg-dark"
+            id="search-addon"
+          >
+            <i className="fas fa-search text-white"></i>
+          </span>
+        </form>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
